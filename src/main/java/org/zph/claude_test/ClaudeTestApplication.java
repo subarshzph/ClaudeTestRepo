9308c81b3425b4842639d430820eb459ae9e174b
subarsh.zph <subarsh.thapa@zakipoint.com>
2026-04-29 16:58:59 +0545

    intial commit

---
+++ src/main/java/org/zph/claude_test/ClaudeTestApplication.java
@@ -0,0 +1,13 @@
+package org.zph.claude_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaudeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaudeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zph/claude_test/ClaudeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zph.claude_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaudeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
